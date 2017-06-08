7ae591d1f45d9c401c7b3b3284dda083580dec00
Gary Houston <ghouston@arglist.com>
2017-06-08 13:48:56 +1000

    Add support for multiple images when MPF is present.

--- exif44repack/exif44repack.go
+++ exif44repack/exif44repack.go
@@ -4,15 +4,16 @@ import (
 	"errors"
 	"fmt"
 	exif "github.com/garyhouston/exif44"
-	"io"
-	"io/ioutil"
 	jseg "github.com/garyhouston/jpegsegs"
 	tiff "github.com/garyhouston/tiff66"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
 )
 
-func processTIFF(infile io.Reader, outfile io.Writer) error {
+// Process a TIFF file.
+func processTIFF(outfile io.Writer, infile io.Reader) error {
 	buf, err := ioutil.ReadAll(infile)
 	if err != nil {
 		return err
@@ -37,12 +38,14 @@ func processTIFF(infile io.Reader, outfile io.Writer) error {
 	return err
 }
 
-func processJPEG(infile io.ReadSeeker, outfile io.WriteSeeker) error {
-	scanner, err := jseg.NewScanner(infile)
+// Process a single image in a JPEG file. A file using Multi-Picture
+// Format will contain multiple images.
+func processImage(writer io.WriteSeeker, reader io.ReadSeeker, mpfProcessor jseg.MPFProcessor) error {
+	scanner, err := jseg.NewScanner(reader)
 	if err != nil {
 		return err
 	}
-	dumper, err := jseg.NewDumper(outfile)
+	dumper, err := jseg.NewDumper(writer)
 	if err != nil {
 		return err
 	}
@@ -69,15 +72,69 @@ func processJPEG(infile io.ReadSeeker, outfile io.WriteSeeker) error {
 			}
 
 		}
+		if marker == jseg.APP0+2 {
+			_, buf, err = mpfProcessor.ProcessAPP2(writer, reader, buf)
+			if err != nil {
+				return err
+			}
+		}
 		if err := dumper.Dump(marker, buf); err != nil {
 			return err
 		}
-		if marker == jseg.SOS {
-			// Start of scan data, no more metadata expected.
-			_, err := io.Copy(outfile, infile)
+		if marker == jseg.EOI {
+			return nil
+		}
+	}
+}
+
+// State for MPF image iterator.
+type iterData struct {
+	writer     io.WriteSeeker
+	newOffsets []uint32
+}
+
+// Function to be applied to each MPF image.
+func (iter *iterData) MPFApply(reader io.ReadSeeker, index uint32, length uint32) error {
+	if index > 0 {
+		pos, err := iter.writer.Seek(0, io.SeekCurrent)
+		if err != nil {
+			return err
+		}
+		iter.newOffsets[index] = uint32(pos)
+		return processImage(iter.writer, reader, &jseg.MPFCheck{})
+	}
+	return nil
+}
+
+// Process additional images found in the MPF index.
+func processMPFImages(writer io.WriteSeeker, reader io.ReadSeeker, index *jseg.MPFIndex) ([]uint32, error) {
+	var iter iterData
+	iter.writer = writer
+	iter.newOffsets = make([]uint32, len(index.ImageOffsets))
+	index.ImageIterate(reader, &iter)
+	return iter.newOffsets, nil
+}
+
+// Process a JPEG file.
+func processJPEG(writer io.WriteSeeker, reader io.ReadSeeker) error {
+	var mpfIndex jseg.MPFIndexRewriter
+	if err := processImage(writer, reader, &mpfIndex); err != nil {
+		return err
+	}
+	if mpfIndex.Tree != nil {
+		newOffsets, err := processMPFImages(writer, reader, mpfIndex.Index)
+		if err != nil {
+			return err
+		}
+		end, err := writer.Seek(0, io.SeekCurrent)
+		if err != nil {
+			return err
+		}
+		if err = jseg.RewriteMPF(writer, mpfIndex.Tree, mpfIndex.APP2WritePos, newOffsets, uint32(end)); err != nil {
 			return err
 		}
 	}
+	return nil
 }
 
 const (
@@ -125,12 +182,12 @@ func main() {
 	}
 	defer outfile.Close()
 	if fileType == TIFFFile {
-		err = processTIFF(infile, outfile)
+		err = processTIFF(outfile, infile)
 		if err != nil {
 			log.Fatal(err)
 		}
 	} else {
-		err = processJPEG(infile, outfile)
+		err = processJPEG(outfile, infile)
 		if err != nil {
 			log.Fatal(err)
 		}
