646d71929d34b3ea3887a099bfa839567908aaeb
Gary Houston <ghouston@arglist.com>
2017-12-24 15:25:27 +1100

    expand package comment

--- doc.go
+++ doc.go
@@ -1,4 +1,19 @@
 /*
-Package exif44 encodes and decodes Exif metadata in TIFF and JPEG files. See the README in the repository for notes and limitations.
+Package exif44 encodes and decodes Exif metadata in TIFF and JPEG
+files. See the README in the repository for notes and limitations.
+
+The high-level interfaces are Read / ReadFile for read-only processing
+and ReadWrite / ReadWriteFile for read-write processing. These are
+procedures that take a control structure with callbacks for
+user-defined processing. Structuring it this way makes it possible for
+the library itself to handle the details of processing JPEG files that
+contain multiple images using the Multi-Picture Format (MPF), aka
+Multi-Picture Object (MPO). This is a JPEG extension used by various
+cameras to store multiple images in a single file, typically for large
+preview images or steroscopic images.
+
+The high-level interfaces are implemented using lower-level interfaces,
+which in turn using the tiff66 and jpegsegs libraries which are
+packaged separately.
 */
 package exif44
